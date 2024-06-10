eb1cbadfc471218bd6434db172570cd93a228bd4
abc <abcemail.com>
2024-06-10 15:52:50 +0530

    added newfilejenkins

---
+++ newjenkin.groovy
@@ -0,0 +1,21 @@
+pipeline
+{
+    agent any;
+    stages
+    {
+        stage('build')
+        {
+            steps
+            {
+                echo "this is build step"
+            }
+        }
+        stage('deploy')
+        {
+            steps
+            {
+                echo "this is deploy step"
+            }
+        }
+    }
+}
